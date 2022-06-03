7e4a7e39d0cf6e12ac6c0be58d800f0f9cdc1895
NKLam <khoalamvn@gmail.com>
2022-06-03 16:38:11 +1000

    Change db connection for heroku

--- db.js
+++ db.js
@@ -5,14 +5,14 @@ let connection;
 
 // Create a connection to the database
 if (env === 'dev') {
-  connection = mysql.createConnection({
+  connection = mysql.createPool({
     host: process.env.DEV_HOST,
     user: process.env.DEV_USER,
     password: process.env.DEV_PASSWORD,
     database: process.env.DEV_DB,
   });
 } else if (env === 'production') {
-  connection = mysql.createConnection({
+  connection = mysql.createPool({
     host: process.env.PROD_HOST,
     user: process.env.PROD_USER,
     password: process.env.PROD_PASSWORD,
